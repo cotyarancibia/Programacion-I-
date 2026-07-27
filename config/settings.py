8b5b7739a34e11afe2268320b615ff1a514294be
cotyarancibia <ca.arancibia@alumno.um.edu.ar>
2026-07-27 16:32:11 -0300

    Agrega roles de usuario y configuracion JWT

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'drf_spectacular',
     'base',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -158,4 +159,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+],
 }
+
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -16,5 +17,16 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='schema'),
         name='swagger-ui'
-    ),
-]
+        ),
+    path(
+        'api/token/', 
+        TokenObtainPairView.as_view(), 
+        name='token_obtain_pair'
+        ),
+    
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(), 
+        name='token_refresh'
+        ),
+] 
