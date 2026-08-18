d45d83055c9730373c02d53e374845eb87e544e3
cotyarancibia <ca.arancibia@alumno.um.edu.ar>
2026-08-18 11:03:21 -0300

    Agrega permisos de acceso segun roles

---
+++ users/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.role == 'ADMIN'
+        )
+
+
+class IsAdminOrVendedor(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return (
+            request.user.is_authenticated
+            and request.user.role in ['ADMIN', 'VENDEDOR']
+        )
+
+
+class IsCliente(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.role == 'CLIENTE'
+        )
