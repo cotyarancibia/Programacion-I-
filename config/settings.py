9f7abf3b27517afffb80195d70db459ccd835e03
cotyarancibia <ca.arancibia@alumno.um.edu.ar>
2026-08-24 11:46:59 -0300

    Corrige configuracion de SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ def env_list(name, default=''):
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-me')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_bool('DEBUG', True)
